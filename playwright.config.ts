293cab7a65ec35ae384ebc39dfb626d53456e663
kimulaco <kimulaco@gmail.com>
2023-10-06 07:28:37 +0900

    Add CI job

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
   use: {
     baseURL: TEST_URL,
   },
